b2e39401ae6fb2badcf860afb2a3f54dad136b34
Julio <julesnoir@hotmail.fr>
2021-11-23 14:25:07 +0100

    Added Error message when fecthing data

--- src/component/Content.js
+++ src/component/Content.js
@@ -5,17 +5,21 @@ import { useEffect, useState } from "react";
 export default function Content(){
     
     const [data,setData] = useState("");
+    const [Msg,setMsg] = useState("Chargement...");
     const LINKS = ["/Personnages","/Planetes","/Films","/Especes","/Vehicules","/Vaisseaux"];
 
     useEffect(()=>{
         fetch("https://swapi.dev/api").then((response) =>
         response.json()).then(function(res){
             setData(res);
-        },[]);
-    });
+        }).catch(function(error){
+            setMsg(() => "Somethigng went wrong : " + error)
+        });
+    },[]);
     return(  
         <section className="card-style">
             <h2>Selection</h2>
+            {!data && <p>{Msg}</p>}
             <ul>
             {Object.keys(data).map((x,i) => <li key={i}> <Link to={LINKS[i]}> {x} </Link> </li>)}
             </ul>
--- src/component/GetGlobalInfoCard.js
+++ src/component/GetGlobalInfoCard.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import { Link } from "react-router-dom";
 
 export default function GetGlobalInfoCard(props){
     const [isLoaded,setIsLoaded] = useState(false);
@@ -6,27 +7,36 @@ export default function GetGlobalInfoCard(props){
     const [itemsData,setItemsData] = useState([]);
     const [nxtPage,setNxtPage] = useState("");
     const [prvPage,setPrvPage] = useState("");
+    const [message,setMessage] = useState("");
+    const URLDATA = [];
     useEffect(() => {
-        fetch(url).then((response) => response.json())
+        fetch(url).then((response) => {
+            setMessage(() => "Chargement en cours...")
+            response.json();
+        })
         .then(function(data){
             console.log(data);
             setItemsData(data.results);
             setNxtPage(data.next);
             setPrvPage(data.previous)
             setIsLoaded(true);
-            console.log(nxtPage);
-            console.log(prvPage);
-        })
+        }).catch( function(error){
+            setMessage(() => "Something went wrong : " + error);
+        }
+        )
     },[url,nxtPage,prvPage])
     return (
         <article className="card-style">
             <h2>{props.title}</h2>
             {
-                (!isLoaded) && <p>...Loading...</p>
+                (!isLoaded) && <p>{message}</p>
             }
+            <ul>
                 {
-                   (itemsData) && itemsData.map((x) => <ul>{(Object.keys(x).map((y,i) => <li key={i}><b>{y}</b> : {x[y]} </li>))}</ul>)
+                   // Code to show all info => (itemsData) && itemsData.map((x) => <ul>{(Object.keys(x).map((y,i) => <li key={i}><b>{y}</b> : {x[y]} </li>))}</ul>)
+                    (itemsData) && itemsData.map((x,i) => <li key={i}>{<Link to="">{x.name}</Link>}</li>)
                 }
+            </ul>
             {
             prvPage ?
                 <button className="btn btn-light" onClick={() => setUrl(prvPage)}> Page Précédente</button>
